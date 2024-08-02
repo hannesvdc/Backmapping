fa7a1519a2d5256546c5156bca84f686bc407c39
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-08-01 20:01:03 -0400

    Implemented Ramachandran Angle computations for proteins, based on my torsion angle calculator.

--- 6kn2/solvated/Ramachandran.py
+++ 6kn2/solvated/Ramachandran.py
@@ -18,3 +18,43 @@ def __compute_torsions__(A, B, C, D):
     phi = np.arctan2((-1)*yp, (-1)*xp)
 
     return phi
+
+class Ramachandran:
+    def __init__(self, amino_acids):
+        self.amino_acids = amino_acids
+        self.lens = {"GLY": 9, 
+                     "PHE": 20, 
+                     "ARG": 24, 
+                     "SER": 11, 
+                     "PRO": 14, 
+                     "CYS": 10}
+
+    # We assume that x is a (N_data, N_atoms, 3) - numpy tensor
+    def computeDihedrals(self, x):
+        ram_angles = []
+
+        total_len = 0
+        for i in range(len(self.amino_acids)):
+            N = x[:, total_len + 0, :]
+            Ca= x[:, total_len + 1, :]
+            C = x[:, total_len + 2, :]
+
+            # Boundary Conditions
+            if i == 0:
+                Cprev = x[:, 4, :] # This is the C in the CH3 initial group
+            else:
+                Cprev = x[: total_len - self.lens[self.amino_acids[i-1]] + 2, :] # C atom from the previous amino-acid
+
+            if i == len(self.amino_acids)-1:
+                Nnext = x[:, total_len + 4, :] # This is the terminal N atom
+            else:
+                Nnext = x[:, total_len + self.lens[self.amino_acids[i]] + 0, :]
+
+            phi = __compute_torsions__(Cprev, N, Ca, C) # According to Wim, Ca must be the 'C' atom on the central bond
+            psi = __compute_torsions__(Nnext, C, Ca, N) # So both looking directions are opposite
+            ram_angles.append((phi, psi))
+
+            # Bookkeeping for the next amino-acid
+            total_len = total_len + self.lens[self.amino_acids[i]]
+		
+        return ram_angles
